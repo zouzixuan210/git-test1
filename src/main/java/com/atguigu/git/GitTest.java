b66a95850fcf31f2a4d458fbdfad5b4a9f3e1f0e
zzx <2858122841@qq.com>
2021-06-02 21:48:20 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git!2");
         System.out.println("Hello Git!3");
         System.out.println("Hello Git!4");
+        System.out.println("push Git!4");
     }
 }
